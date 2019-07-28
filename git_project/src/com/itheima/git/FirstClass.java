8834006b61fe44a9c362ff89866cc286d070bffc
lisi <lisi@itcast.cn>
2019-07-28 21:43:21 +0800

    把本地工程添加到仓库中

---
+++ git_project/src/com/itheima/git/FirstClass.java
@@ -0,0 +1,11 @@
+package com.itheima.git;
+
+public class FirstClass {
+
+    public static void main(String[] args) {
+        System.out.println("你好,刘小胖");
+        System.out.println("你真胖");
+        System.out.println("是真的胖");
+        System.out.println("是真的真的胖");
+    }
+}
